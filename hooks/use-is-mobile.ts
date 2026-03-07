f6ad1bba8f6238c5c682c65be81f2455b2cd9bfd
DW <55995125+ogdakke@users.noreply.github.com>
2026-03-07 14:36:40 +0200

    some changes to ismobile hook

--- hooks/use-is-mobile.ts
+++ hooks/use-is-mobile.ts
@@ -2,11 +2,16 @@ import useMediaQuery from "./use-media-query";
 
 export function useIsMobile() {
   const isNarrow = useMediaQuery("(max-width: 640px)");
-  const isCoarse = useMediaQuery("(any-pointer: coarse)");
+  const isLandscape = useMediaQuery("(orientation: landscape)");
+  const isCoarse = useMediaQuery("(any-pointer: coarse)") && navigator.maxTouchPoints > 1;
+  // if landscape, only consider coarse pointer and touch points
+  if (isLandscape) {
+    return isCoarse;
+  }
   // chrome devtools triggers pointer: fine when picking an element, which is annoying, so in DEV, just look at narrow
   return isNarrow && (import.meta.env.DEV || isCoarse);
 }
 
 export function useIsTouch() {
-  return useMediaQuery("(any-pointer: coarse)");
+  return useMediaQuery("(any-pointer: coarse)") && navigator.maxTouchPoints > 1;
 }
