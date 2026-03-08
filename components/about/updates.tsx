28ebd81dd9cd2773b95685badebe09c866ca9f98
DW <55995125+ogdakke@users.noreply.github.com>
2026-03-08 21:48:51 +0200

    add: entry about upscaling in updates

--- components/about/updates.tsx
+++ components/about/updates.tsx
@@ -15,6 +15,13 @@ export function Updates({ id }: { id?: string }) {
     <section id={id}>
       <h1>Updates</h1>
       <Update>
+        <UpdatesTitle date="2026-03-08">Upscaling</UpdatesTitle>
+        <p>
+          Upscale images and videos, fully locally. Find the upscale button in the context menu, and
+          give it a go!
+        </p>
+      </Update>
+      <Update>
         <UpdatesTitle date="2026-03-07">Mobile context menu</UpdatesTitle>
         <p>Now you can do more actions that were already available on desktop on mobile as well!</p>
         <figure>
