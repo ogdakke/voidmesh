1fbdc51cae99274546378850212166543c369edb
DW <55995125+ogdakke@users.noreply.github.com>
2026-03-22 02:20:12 +0200

    change: use dpr for scale

--- renderer/canvas-renderer.ts
+++ renderer/canvas-renderer.ts
@@ -2021,7 +2021,7 @@ export class InfiniteCanvasRenderer {
         encoder,
         targetView,
         "debug-ui",
-        1, // world-space scale
+        dpr,
         undefined,
         {
           offsetX: viewport.offset.x,
